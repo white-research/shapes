a6397c7cf44bc15e54800380aa1b330ad4c88f0e
Dominic Office <dewhite4@gmail.com>
2015-01-23 18:09:53 -0500

    Initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shapes',
+      version='0.1',
+      description='Tools for analyzing geometric morphometic shape',
+      url='http://github.com/DominicWhite/shapes',
+      author='Dominic White',
+      author_email='dewhite4@gmail.com',
+      license='MIT',
+      packages=['shapes'],
+      zip_safe=False)
