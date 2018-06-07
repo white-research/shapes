5e71ffa12cbf97bec977e4d4f859b0ef069efcd3
Dominic White <dewhite4@gmail.com>
2018-06-06 23:56:05 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+v='0.1.3'
+
 setup(name='shapes',
-    version='0.1.2.2',
+    version = v,
     description='Tools for analyzing geometric morphometic shape',
     url='http://github.com/DominicWhite/shapes',
     author='Dominic White',
     author_email='dewhite4@gmail.com',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     license='MIT',
     packages=['shapes'],
     install_requires=[
